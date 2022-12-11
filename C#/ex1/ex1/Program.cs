fe0cd41963233e67cb40d4647288c724557df15b
Erick Knoblich <Erick.Larratea.Knoblich@gmail.com>
2022-12-10 21:05:41 -0300

    Adicionar arquivos de projeto.

---
+++ C#/ex1/ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HelloWorld.Hello());
+        }
+    }
+
+    public static class HelloWorld
+    {
+        public static string Hello() => "Hello, World!";
+    }
+}
